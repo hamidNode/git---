177f275048730eeb674ea8cf6857322cc248f323
ravenDark <hamidraven13@gamil.com>
2024-01-24 17:26:06 +0330

    a.js has been added and commited!!1

---
+++ a.js
@@ -0,0 +1,9 @@
+function rev(str){
+    return str.reverse()
+    // return str.split('').reverse().join('')
+}
+
+let a= ['a','s','d']
+// console.log(rev('I Am Raven'))
+console.log(a)
+console.log(rev (a))
